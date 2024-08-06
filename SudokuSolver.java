23d426d46ba401f3a019016492077b02379652ca
VaishnaviMorde38 <163237376+VaishnaviMorde38@users.noreply.github.com>
2024-08-06 20:23:21 +0530

    Add files via upload

---
+++ SudokuSolver.java
@@ -0,0 +1,110 @@
+public class SudokuSolver {
+
+    public static void main(String[] args) {
+        int[][] board = {
+            {5, 3, 0, 0, 7, 0, 0, 0, 0},
+            {6, 0, 0, 1, 9, 5, 0, 0, 0},
+            {0, 9, 8, 0, 0, 0, 0, 6, 0},
+            {8, 0, 0, 0, 6, 0, 0, 0, 3},
+            {4, 0, 0, 8, 0, 3, 0, 0, 1},
+            {7, 0, 0, 0, 2, 0, 0, 0, 6},
+            {0, 6, 0, 0, 0, 0, 2, 8, 0},
+            {0, 0, 0, 4, 1, 9, 0, 0, 5},
+            {0, 0, 0, 0, 8, 0, 0, 7, 9}
+        };
+
+        if (solveSudoku(board)) {
+            System.out.println("Sudoku solved successfully:");
+            printBoard(board);
+        } else {
+            System.out.println("No solution exists.");
+        }
+    }
+
+    public static boolean solveSudoku(int[][] board) {
+        int row = -1;
+        int col = -1;
+        boolean isEmpty = true;
+
+        // Find first empty cell
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (board[i][j] == 0) {
+                    row = i;
+                    col = j;
+                    isEmpty = false;
+                    break;
+                }
+            }
+            if (!isEmpty) {
+                break;
+            }
+        }
+
+        // If no empty cell found, puzzle solved
+        if (isEmpty) {
+            return true;
+        }
+
+        // Try numbers 1 through 9
+        for (int num = 1; num <= 9; num++) {
+            if (isSafe(board, row, col, num)) {
+                board[row][col] = num;
+
+                // Recursively solve rest of the board
+                if (solveSudoku(board)) {
+                    return true;
+                } else {
+                    // Backtrack
+                    board[row][col] = 0;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static boolean isSafe(int[][] board, int row, int col, int num) {
+        // Check if number num is not already present in current row, current column and current 3x3 subgrid
+        return !usedInRow(board, row, num) &&
+               !usedInColumn(board, col, num) &&
+               !usedInBox(board, row - row % 3, col - col % 3, num);
+    }
+
+    public static boolean usedInRow(int[][] board, int row, int num) {
+        for (int col = 0; col < board.length; col++) {
+            if (board[row][col] == num) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean usedInColumn(int[][] board, int col, int num) {
+        for (int row = 0; row < board.length; row++) {
+            if (board[row][col] == num) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean usedInBox(int[][] board, int boxStartRow, int boxStartCol, int num) {
+        for (int row = 0; row < 3; row++) {
+            for (int col = 0; col < 3; col++) {
+                if (board[row + boxStartRow][col + boxStartCol] == num) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static void printBoard(int[][] board) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
